442c8e92ee6883e24fb473120c7fc1059a55f806
Steven MacCoun <theraccoun@gmail.com>
2013-02-17 02:24:10 -0700

    Added switchable tools with toggle click

--- bbs.js
+++ bbs.js
@@ -1,101 +1,47 @@
 var MAX_SORT_ELEMENTS = 5;
 
-var isStartDragging = false;
-
-var startMouseX;
-var startMouseY;
-
-var elementDragX;
-var elementDragY;
-var elementDragZ0;
-
-var dragElement;
+var sortliststate = {};
 
 
 function init(){
     drawInitialSortElements();
-
-    document.onmousedown = OnMouseDown;
-    document.onmouseup = OnMouseUp;
-}
-
-function OnMouseDown(e){
-
-    // Handle IE case for event
-    if (e == null)
-        e = window.event;
-
-    var target = e.target != null ? e.target : e.srcElement;
-
-    if(target.className == 'sortElement'){
-
-        isStartDragging = true;
-
-        startMouseX = e.clientX;
-        startMouseY = e.clientY;
-
-        elementDragX = ExtractNumber(target.style.left);
-        elementDragY = ExtractNumber(target.style.top);
-
-//        console.log("first X: " + elementDragX);
-//        console.log("first Y: " + elementDragY);
-
-        dragElement = target;
-
-        elementDragZ0 = target.style.zIndex;
-        target.style.zIndex = 1000;
-
-        document.onmousemove = MouseDrag;
-
-        document.body.focus();
-
-        return false;
-    }
-}
-
-function MouseDrag(e){
-
-    if (e == null)
-        var e = window.event;
-
-    dragElement.style.left = (elementDragX + e.clientX - startMouseX) + 'px';
-    dragElement.style.top = (elementDragY + e.clientY - startMouseY) + 'px';
-
-    console.log("elemetnDragX: " + elementDragX);
-    console.log("new clientX: " + e.clientX);
-    console.log("startMouseX: " + startMouseX);
-    console.log(dragElement.style.left);
-}
-
-function OnMouseUp(e){
-    if (dragElement != null)
-    {
-        dragElement.style.zIndex = elementDragZ0;
-
-        // we're done with these events until the next OnMouseDown
-        document.onmousemove = null;
-        document.onselectstart = null;
-        dragElement.ondragstart = null;
-
-        // this is how we know we're not dragging
-        dragElement = null;
-    }
 }
 
 function drawInitialSortElements(){
-    $('#bucketMaster').children('div').each(function(i) {
-        var rand = Math.floor(Math.random()*11);
-        var $sortElement = $("<div class='sortElement'>" + rand + "</div>");
+    sortliststate.order = new Array();
 
-        $(this).append($sortElement);
-    });
+
+        for(var i=0; i < MAX_SORT_ELEMENTS; ++i){
+            var $bucket = jQuery('<div/>', {
+                id: i,
+                class: 'bucket'
+            });
+            var $switchToolHead = $("<div class='switchToolHead'></div>");
+            $switchToolHead.appendTo($bucket);
+            alert($switchToolHead.parent().attr('id'));
+            $switchToolHead.button().click(function(event){
+                $(this).toggleClass("showSWTClicked");
+            });
+            $('#bucketMaster').append($bucket);
+        }
+
+        $('#bucketMaster').children('.bucket').each(function(i) {
+
+            var rand = Math.floor(Math.random()*11);
+            sortliststate.order.push(rand);
+            var $sortElement = $("<div class='sortElement'>" + rand + "</div>");
+
+
+            $sortElement.draggable({
+                containment: '#bucketMaster'
+            });
+
+            $(this).append($sortElement);
+        });
 }
 
-function ExtractNumber(value)
-{
-    var n = parseInt(value);
-
-    return n == null || isNaN(n) ? 0 : n;
+function nextIteration(){
+    alert("NEXT ITERATION!" + sortliststate["order"][0]);
 }
 
 $(document).ready(function(){
